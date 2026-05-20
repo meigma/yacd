cff1aa91415be6d3a49a7c00cac778d625b1d608
Joshua Gilman <joshuagilman@gmail.com>
2026-05-20 16:41:22 -0700

    feat(api): add CardanoNetwork CRD draft

---
+++ api/v1alpha1/groupversion_info.go
@@ -0,0 +1,31 @@
+// Package v1alpha1 contains API Schema definitions for the yacd v1alpha1 API group.
+// +kubebuilder:object:generate=true
+// +groupName=yacd.meigma.io
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+var (
+	// SchemeGroupVersion is group version used to register these objects.
+	// This name is used by applyconfiguration generators (e.g. controller-gen).
+	SchemeGroupVersion = schema.GroupVersion{Group: "yacd.meigma.io", Version: "v1alpha1"}
+
+	// GroupVersion is an alias for SchemeGroupVersion, for backward compatibility.
+	GroupVersion = SchemeGroupVersion
+
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+
+	// AddToScheme adds the types in this group-version to the given scheme.
+	AddToScheme = SchemeBuilder.AddToScheme
+)
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion, &CardanoNetwork{}, &CardanoNetworkList{})
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}
